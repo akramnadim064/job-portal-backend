cc9e6e38fcac2f78dae54b9f46fca30031eb9595
Nadim <akramnadim064@gmail.com>
2025-12-28 23:44:25 +0530

    fix cors

--- server.js
+++ server.js
@@ -52,6 +52,47 @@
 
 
 
+// const express = require("express");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+// const connectDB = require("./config/db");
+
+// dotenv.config();
+// connectDB();
+
+// const app = express();
+
+// // middlewares
+// app.use(cors());
+// app.use(express.json());
+
+// // ROUTES
+// app.use("/api/auth", require("./routes/authRoutes"));
+// app.use("/api/profile", require("./routes/profileRoutes"));
+// app.use("/api/jobs", require("./routes/jobRoutes"));
+// app.use("/api/applications", require("./routes/applicationRoutes"));
+// app.use("/api/employer", require("./routes/employerDashboardRoutes"));
+
+// // health check
+// app.get("/", (req, res) => {
+//   res.send("Job Portal API is running...");
+// });
+
+// // DEBUG: catch unmatched routes
+// app.use((req, res) => {
+//   console.log(" UNMATCHED:", req.method, req.originalUrl);
+//   res.status(404).json({ message: "Route not found" });
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+
+
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
@@ -62,8 +103,24 @@ connectDB();
 
 const app = express();
 
-// middlewares
-app.use(cors());
+
+   //CORS CONFIG (IMPORTANT)
+
+app.use(
+  cors({
+    origin: [
+      "https://job-portal-frontend-blush-zeta.vercel.app",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+// allow preflight requests
+app.options("*", cors());
+
+// MIDDLEWARES
 app.use(express.json());
 
 // ROUTES
@@ -73,16 +130,18 @@ app.use("/api/jobs", require("./routes/jobRoutes"));
 app.use("/api/applications", require("./routes/applicationRoutes"));
 app.use("/api/employer", require("./routes/employerDashboardRoutes"));
 
-// health check
+// HEALTH CHECK
 app.get("/", (req, res) => {
   res.send("Job Portal API is running...");
 });
 
-// DEBUG: catch unmatched routes
+// DEBUG: CATCH UNMATCHED ROUTES
 app.use((req, res) => {
-  console.log("❌ UNMATCHED:", req.method, req.originalUrl);
+  console.log("UNMATCHED:", req.method, req.originalUrl);
   res.status(404).json({ message: "Route not found" });
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
