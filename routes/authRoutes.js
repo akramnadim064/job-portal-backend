f1aab4353ecf40dd9848720d702a239cebd8c7fa
Nadim <akramnadim064@gmail.com>
2025-12-29 01:09:44 +0530

    first

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,23 @@
+// // const express = require("express");
+// // const router = express.Router();
+
+// // const { registerUser, loginUser } = require("../controllers/authController");
+
+// // // REGISTER
+// // router.post("/register", registerUser);
+
+// // // LOGIN
+// // router.post("/login", loginUser);
+
+// // module.exports = router;
+
 // const express = require("express");
 // const router = express.Router();
 
-// const { registerUser, loginUser } = require("../controllers/authController");
+// const {
+//   registerUser,
+//   loginUser,
+// } = require("../controllers/authController");
 
 // // REGISTER
 // router.post("/register", registerUser);
@@ -11,18 +27,22 @@
 
 // module.exports = router;
 
+
+
+
+
+//no use
 const express = require("express");
 const router = express.Router();
+const { register, login } = require("../controllers/authController");
 
-const {
-  registerUser,
-  loginUser,
-} = require("../controllers/authController");
+/* ✅ Explicitly allow OPTIONS */
+router.options("*", (req, res) => {
+  res.sendStatus(204);
+});
 
-// REGISTER
-router.post("/register", registerUser);
-
-// LOGIN
-router.post("/login", loginUser);
+/* Routes */
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
